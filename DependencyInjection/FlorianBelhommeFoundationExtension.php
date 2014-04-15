1de91d6119f1752ee8c20b38056040837af27395
solune <florian.belhomme@gmail.com>
2014-04-15 11:16:04 +0200

    Better dependency check and error messages

--- DependencyInjection/FlorianBelhommeFoundationExtension.php
+++ DependencyInjection/FlorianBelhommeFoundationExtension.php
@@ -50,18 +50,33 @@ class FlorianBelhommeFoundationExtension extends Extension implements PrependExt
         $config = $this->processConfiguration(new Configuration(), $configs);
         
         // This will change the Twig configuration if needed (default template path for exemple)
-        if ((isset($bundles['TwigBundle'])) && ($config['theme']['form'])) {
-            $container->prependExtensionConfig('twig', array('form'  => array('resources' => array($config['template']['form']))));
+        if ($config['theme']['form']) {
+            if (isset($bundles['TwigBundle'])) {
+                $container->prependExtensionConfig('twig', array('form' => array('resources' => array($config['template']['form']))));
+            }
+            else {
+                throw new InvalidConfigurationException('You need to enable Twig Bundle to theme form or set the configuration of florian_belhomme_foundation:theme:form to false');
+            }
         }
 
         // This will change the KnpMenu configuration if needed (default template path for exemple)
-        if ((isset($bundles['TwigBundle'])) && (isset($bundles['KnpMenuBundle'])) && ($config['theme']['knp_menu'])) {
-            $container->prependExtensionConfig('knp_menu', array('twig' => array('template' => $config['template']['knp_menu'])));
+        if ($config['theme']['knp_menu']) {
+            if ((isset($bundles['TwigBundle'])) && (isset($bundles['KnpMenuBundle'])) ) {
+                $container->prependExtensionConfig('knp_menu', array('twig' => array('template' => $config['template']['knp_menu'])));
+            }
+            else {
+                throw new InvalidConfigurationException('You need to enable Twig Bundle and KNP Menu Bundle to theme form or set the configuration of florian_belhomme_foundation:theme:knp_menu to false');
+            }
         }
 
         // This will change the KnpPagination configuration if needed (default template path for exemple)
-        if ((isset($bundles['TwigBundle'])) && (isset($bundles['KnpPaginatorBundle'])) && ($config['theme']['knp_paginator'])) {
-            $container->prependExtensionConfig('knp_paginator', array('template' => array('pagination' => $config['template']['knp_paginator'])));
+        if ($config['theme']['knp_paginator']) {
+            if ((isset($bundles['TwigBundle'])) && (isset($bundles['KnpPaginatorBundle']))) {
+                $container->prependExtensionConfig('knp_paginator', array('template' => array('pagination' => $config['template']['knp_paginator'])));
+            }
+            else {
+                throw new InvalidConfigurationException('You need to enable Twig Bundle and KNP Paginator Bundle to theme form or set the configuration of florian_belhomme_foundation:theme:knp_paginator to false');
+            }
         }
     }
 }
