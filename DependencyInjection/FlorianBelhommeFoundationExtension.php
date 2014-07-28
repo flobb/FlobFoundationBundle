94365aeade172b546314550cd7edee63d64f22ed
Florian Belhomme <florian.belhomme@gmail.com>
2014-07-28 13:31:57 +0200

    Remove comments

--- DependencyInjection/FlorianBelhommeFoundationExtension.php
+++ DependencyInjection/FlorianBelhommeFoundationExtension.php
@@ -9,12 +9,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class FlorianBelhommeFoundationExtension extends Extension implements PrependExtensionInterface
 {
     
