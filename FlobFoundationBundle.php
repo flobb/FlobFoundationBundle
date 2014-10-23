1e497ed8ad626bff018b611869218399eec5a331
Florian Belhomme <florian.belhomme@sensiolabs.com>
2014-10-23 13:51:11 +0200

    Namespace update

---
+++ FlobFoundationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Flob\Bundle\FoundationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FlobFoundationBundle extends Bundle
+{
+}
--- FlorianBelhommeFoundationBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FlorianBelhomme\Bundle\FoundationBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FlorianBelhommeFoundationBundle extends Bundle
-{
-}
